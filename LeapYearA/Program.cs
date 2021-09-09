3719942447b5692ba60fd24333f451b2591577ce
Thomas <hascitu@protonmail.com>
2021-09-09 18:42:06 +0200

    Added input

--- LeapYearA/Program.cs
+++ LeapYearA/Program.cs
@@ -6,6 +6,14 @@ namespace LeapYear
 
     public static void Main(string[] args)
     {
+      Console.WriteLine("Enter a year to check if it is a leap year:");
+      
+      string yearStr = Console.ReadLine();
+      int year = Convert.ToInt32(yearStr);
+
+      LeapYearHelper helper = new LeapYearHelper();
+      
+      Console.WriteLine(helper.IsLeapYear(year) ? "yay" : "nay");
       
     }
 
