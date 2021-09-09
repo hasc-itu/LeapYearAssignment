81be83942346c84c1c15801faa94b7299fd747f4
Thomas <hascitu@protonmail.com>
2021-09-09 13:45:58 +0200

    Project Setup

---
+++ LeapYearA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeapYearA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
